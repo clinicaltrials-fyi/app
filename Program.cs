a4efa10e56cbd4dba54f75701e0b021487e47209
Rob Relyea <rob@relyeas.net>
2023-02-12 07:50:22 -0800

    rename project

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using clinical_trials_site;
+using ClinicalTrials;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
