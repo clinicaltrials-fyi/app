631b033a33a75beb76a330df9185665fcb85facd
Rob Relyea <rob@relyeas.net>
2024-03-26 11:14:36 -0700

    add mobile app projects

---
+++ ClinicalTrials.Apps/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicalTrials.Apps
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ClinicalTrials.Apps/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ClinicalTrials.Apps
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
